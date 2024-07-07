2cfea2421cd9fe79fe625f1a63355fe7a54ab611
MarharytaFilipovych <150688071+MarharytaFilipovych@users.noreply.github.com>
2024-07-07 21:06:30 +0300

    adjustments

--- Task1.py
+++ Task1.py
@@ -1,40 +1,40 @@
 import random
 import numpy as np
+def sort_eigenvalues_eigenvectors(eigenvalues, matrix):
+    sort_indices = np.argsort(eigenvalues)[::-1]
+    eigenvalues = eigenvalues[sort_indices]
+    matrix = matrix[:, sort_indices]
+    return eigenvalues, matrix
 
+def get_E(eigenvalues):
+    singular_values = np.sqrt(eigenvalues)
+    E = np.diag(singular_values)
+    return E, singular_values
 
-def SVD(matrix):
-    A = np.array(matrix)
+def SVD(A):
     if A.shape[0] >= A.shape[1]:
         ATA = np.dot(A.T, A)
         eigenvalues, V = np.linalg.eig(ATA)
-        sort_indices = np.argsort(eigenvalues)[::-1]
-        eigenvalues = eigenvalues[sort_indices]
-        V = V[:, sort_indices]
-        singular_values = np.sqrt(eigenvalues)
-        E = np.diag(singular_values)
+        eigenvalues, V = sort_eigenvalues_eigenvectors(eigenvalues, V)
+        E, singular_values = get_E(eigenvalues)
         U = np.dot(A, V) / singular_values
     else:
         AAT = np.dot(A, A.T)
         eigenvalues, U = np.linalg.eig(AAT)
-        sort_indices = np.argsort(eigenvalues)[::-1]
-        eigenvalues = eigenvalues[sort_indices]
-        U = U[:, sort_indices]
-        singular_values = np.sqrt(eigenvalues)
-        E = np.diag(singular_values)
+        eigenvalues, U = sort_eigenvalues_eigenvectors(eigenvalues, U)
+        E, singular_values = get_E(eigenvalues)
         V = np.dot(A.T, U) / singular_values
-
     return U, E, V.T
+
 def check_SVD(U, E, VT, matrix):
     reconstructed_matrix = np.dot(np.dot(U, E), VT)
     if np.allclose(matrix, reconstructed_matrix):
         print("Correct!")
-        return True
-    print("Incorrect!")
-    return False
+    else:
+        print("Incorrect!")
 
-#matrix = np.array([[5, 1], [8, 1]])
-rows = random.randint(2, 4)
-columns = random.randint(2, 4)
+rows = random.randint(1, 15)
+columns = random.randint(1, 15)
 matrix = np.random.randint(0, 256, size=(rows, columns))
 U, E, VT = SVD(matrix)
 print("Original Matrix:")
